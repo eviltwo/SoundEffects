d61d54e2a432a660b22e685460c0296ac62be8c9
DAIKI <daiki.evilone@gmail.com>
2024-07-29 12:22:49 +0900

    Added look sample scene

---
+++ SoundEffects/Assets/Samples/Scenes/LoopSample.cs
@@ -0,0 +1,30 @@
+using SoundEffects;
+using UnityEngine;
+
+public class LoopSample : MonoBehaviour
+{
+    [SerializeField]
+    private float _interval = 1.0f;
+
+    [SerializeField]
+    private string[] _soundKeys = default;
+
+    private float _elapsedTime;
+    private int _nextIndex;
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime > _interval)
+        {
+            _elapsedTime = 0;
+            PlaySound(_soundKeys[_nextIndex]);
+            _nextIndex = (_nextIndex + 1) % _soundKeys.Length;
+        }
+    }
+
+    private void PlaySound(string key)
+    {
+        SoundEffectManager.Player.PlayOneShot(key, 1);
+    }
+}
